cb2f6d9fe0ede35f7e5f98a1aed72ed4c22d0ddf
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-20 15:17:34 -0600

    add meals controller and further restructure routes

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,5 +1,48 @@
 const SavedMeal = require('../models/savedMeal');
+const User = require('../models/user');
+const ExpressError = require('../utils/ExpressError');
 
 module.exports.index = (req, res) => {
   res.render('meals/index')
+};
+
+module.exports.renderSearch = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if(!user){
+    res.render('meals/search')
+  }
+  res.render('meals/search', { user })
+};
+
+module.exports.saveMeal = async (req, res, next) => {
+  if(!req.body.savedMeal) throw new ExpressError('Invalid Meal Data', 400);
+  const savedMeal = new SavedMeal(req.body.savedMeal);
+  const user = await User.findById(req.user._id);
+  await savedMeal.save();
+  user.meals.push(savedMeal._id);
+  await user.save();
+  req.flash('success', 'Meal Saved!')
+};
+
+module.exports.renderMyMeals = async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  const mealIds = user.meals;
+  let meals = [];
+
+  for(let mealId of mealIds){
+    const savedMeal = await SavedMeal.findById(mealId);
+    meals.push(savedMeal)
+  }
+
+  res.render('meals/myMeals', { meals })
+};
+
+module.exports.showMeal = async (req, res) => {
+  const meal = await SavedMeal.findById(req.params.id)
+
+  res.render('meals/show', { meal });
+};
+
+module.exports.renderMealPlan = (req, res) => {
+  res.render('meals/mealPlan')
 };
