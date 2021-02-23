599f10f68669fdb601aa1df791c35e7a232d5e2c
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-22 19:18:27 -0600

    update user model

--- models/user.js
+++ models/user.js
@@ -17,107 +17,86 @@ const UserSchema = new Schema({
   sundaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   sundaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   sundaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   mondaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   mondaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   mondaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   tuesdaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   tuesdaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   tuesdaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   wednesdaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   wednesdaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   wednesdaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   thursdaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   thursdaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   thursdaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   fridaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   fridaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   fridaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   saturdaybreakfast: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   saturdaylunch: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
   saturdaydinner: {
     type: Schema.Types.ObjectId,
     ref: 'savedMeal',
-    default: '603432126ed81f4e881fc183'
   },
 });
 
