c6626f9fb6f564ed20983279d09af81df67fe0ef
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-28 09:21:55 -0600

    add mongo sanitize

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const meals = require('./routes/meals');
 const users = require('./routes/users');
@@ -36,6 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: 'dragonsarereal',
